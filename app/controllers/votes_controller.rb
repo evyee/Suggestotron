6b4a62a12eb2276b3a99bf7e1ba7b910c38d0c0f
evyee <jvalieva@yahoo.com>
2013-08-24 16:10:01 -0700

    Downvote button and heading change

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,8 +1,14 @@
 class VotesController < ApplicationController
-	def create
+	def create 
 		topic = Topic.find(params[:topic_id])
 		vote = topic.votes.build
 		vote.save!
+		redirect_to(topics_path)
+	end
+	def destroy
+		
+		vote = Vote.find(params[:id])
+		vote.destroy!
 		redirect_to(topics_path)
 	end
 end
